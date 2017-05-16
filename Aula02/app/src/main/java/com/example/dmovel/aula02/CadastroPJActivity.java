34a9d49d93a4242435173d6be8e5fc26f9d9e63f
weuller <weullermarcos@hotmail.com>
2017-05-15 21:19:27 -0300

    Criando novas telas

---
+++ Aula02/app/src/main/java/com/example/dmovel/aula02/CadastroPJActivity.java
@@ -0,0 +1,13 @@
+package com.example.dmovel.aula02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroPJActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_pj);
+    }
+}
--- Aula02/app/src/main/java/com/example/dmovel/aula02/MainActivity.java
+++ Aula02/app/src/main/java/com/example/dmovel/aula02/MainActivity.java
@@ -10,35 +10,93 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button cadatrarPf;
+    //instanciando botões
+    private Button cadastrarPF;
+    private Button cadastrarPJ;
+    private Button cadastrarConta;
+
+    //metodos de criação da tela
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cadatrarPf = (Button) findViewById(R.id.main_cadastra_pf);
+        Log.d("MAIN","PASSANDO PELO ONCREATE");
 
-        cadatrarPf.setOnClickListener(new View.OnClickListener() {
+        //recuperando informações dos botões
+        cadastrarPF = (Button) findViewById(R.id.main_cadastrar_pf);
+        cadastrarPJ = (Button) findViewById(R.id.main_cadastrar_pj);
+        cadastrarConta = (Button) findViewById(R.id.main_cadastrar_conta);
+
+        //evento para o botão cadastraPF
+        cadastrarPF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CadastroPFActivity.class);
                 startActivity(intent);
             }
         });
+
+        //evento para o botão cadastraPJ
+        cadastrarPJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, CadastroPJActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cadastrarConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, CadastrarConta.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("MAIN","PASSANDO PELO ONSTART");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("MAIN","PASSANDO PELO ONSRESUME");
+    }
+
+
+    //metodos de destruir a tela:
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        Log.d("TAG1","PASSANDO PELO ONPAUSE");
+        Log.d("MAIN","PASSANDO PELO ONPAUSE");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("MAIN","PASSANDO PELO ONPSTOP");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        Log.d("TAG2","");
+        Log.d("MAIN","PASSANDO PELO ONDESTROY");
     }
+
 }
