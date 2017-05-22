ca60773cc63a70b96ad539382a8a99d23d7cd7b0
Weuller Marcos <weullermarcos@hotmail.com>
2017-05-22 19:11:01 -0300

    adicionando log

--- Aula02/app/src/main/java/com/example/dmovel/aula02/CadastrarConta.java
+++ Aula02/app/src/main/java/com/example/dmovel/aula02/CadastrarConta.java
@@ -2,6 +2,7 @@ package com.example.dmovel.aula02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class CadastrarConta extends AppCompatActivity {
 
@@ -9,5 +10,42 @@ public class CadastrarConta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_conta);
+
+        Log.d("CONTA","PASSANDO PELO ONCREATE");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d("CONTA","PASSANDO PELO ONSTART");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("CONTA","PASSANDO PELO ONRESUME");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("CONTA","PASSANDO PELO ONPAUSE");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("CONTA","PASSANDO PELO ONPSTOP");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("CONTA","PASSANDO PELO ONDESTROY");
     }
 }
