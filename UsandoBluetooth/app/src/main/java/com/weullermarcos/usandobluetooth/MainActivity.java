1226245b8f13333e3a25fda95337da7eb7fa40af
Weuller Marcos da Silva Santos <weuller@redecfa.org.br>
2017-08-01 16:10:46 -0300

    buscando bluetooth

--- UsandoBluetooth/app/src/main/java/com/weullermarcos/usandobluetooth/MainActivity.java
+++ UsandoBluetooth/app/src/main/java/com/weullermarcos/usandobluetooth/MainActivity.java
@@ -1,5 +1,7 @@
 package com.weullermarcos.usandobluetooth;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -7,21 +9,32 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.jar.Manifest;
 
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_FINE_LOCATION = 0;
 
     ListView lstDevices;
+    private final int REQUEST_ENABLE_BT = 7;
+    private ArrayAdapter<String> mArrayAdapter;
+    Dialog mProgressDlg;
+
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,46 +43,87 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
         //cria lista de dispositivos
         lstDevices = (ListView) findViewById(R.id.lstDevices);
+        mArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        lstDevices.setAdapter(mArrayAdapter);
 
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
         filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
         filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
 
-        BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        BroadcastReceiver mReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
 
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1);
-                ProgressDialog progress = new ProgressDialog(context);
                 String action = intent.getAction();
 
-                if(BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)){
+                if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)){
 
-                    progress = ProgressDialog.show(MainActivity.this, "Bluetooth", "Procurando dispositivos...");
-                    progress.show();
+                    final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+
+                    if(state == BluetoothAdapter.STATE_ON){
+                        showToast("ACTION_STATE_CHANGED: STATE_ON");
+                    }
 
                 }
-                else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
-                    progress.dismiss();
+                else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                    showToast("ACTION_DISCOVERY_STARTED");
+
+                    if(mProgressDlg != null && mProgressDlg.isShowing())
+                        mProgressDlg.dismiss();
+
+                    mProgressDlg = ProgressDialog.show(context, "Buscando..", "Buscando dispositivos", false, false);
                 }
-                else if(BluetoothDevice.ACTION_FOUND.equals(action)){
 
-                    BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
 
-                    adapter.add(device.getName() + "\n" + device.getAddress());
+                    mProgressDlg.dismiss();
+                    showToast("ACTION_DISCOVERY_FINISHED");
 
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                    if (device != null){
+
+                        mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                        showToast("Device found = " + device.getName());
+                    }
+
+
+//                    Intent newIntent = new Intent(MainActivity.this, DeviceListActivity.class);
+//                    newIntent.putParcelableArrayListExtra("device.list", mDeviceList);
+//                    startActivity(newIntent);
+                }
+                else if (BluetoothDevice.ACTION_FOUND.equals(action)) {// When discovery finds a device
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//                    mDeviceList.add(device);
+                    mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                    showToast("Device found = " + device.getName());
                 }
             }
         };
 
-        registerReceiver(receiver, filter);
+
+        registerReceiver(mReceiver, filter);
+
+        //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+
+        loadPermissions(android.Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_FINE_LOCATION);
+
+        //inicia a discoberta de novos dispositivos
+        mBluetoothAdapter.startDiscovery();
 
 
-//        Forma 1 de fazer
-//        lista todos os dispositivos bluetooth que já conectaram com o celular
-//
-//        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-//        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+
+//        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
 //
 //        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
 //
@@ -82,31 +136,25 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 //            }
 //        }
 
-
     }
 
-    private void checkBluetooth(){
+    private void loadPermissions(String perm, int requestCode) {
 
-        int hasPermission = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        Log.d("MENSAGEM: ","ENTROU NO LOAD");
 
-        if(hasPermission == PackageManager.PERMISSION_GRANTED){
-            continueDoDiscvery();
+        if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, perm)) {
+                ActivityCompat.requestPermissions(this, new String[]{perm},requestCode);
+            }
         }
-
-        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_COARSE_LOCATION_PERMISSIONS);
-
     }
 
-    private void continueDoDiscvery(){
+    //exibe toast
+    private void showToast(String message){
 
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-        btAdapter.startDiscovery();
-
-    }
-
-    @Override
-    public void onClick(View view) {
-
-
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
+
+
