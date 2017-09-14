0f1d6a898e9a7b6ca1a228c0ae2b5655932278bc
Weuller Marcos <weullermarcos@MBP-de-Weuller.lan>
2017-09-13 23:11:17 -0300

    adicionando projeto de fragments

---
+++ UsandoFragment/app/src/main/java/com/weullermarcos/usandofragment/MainActivity.java
@@ -0,0 +1,22 @@
+package com.weullermarcos.usandofragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
