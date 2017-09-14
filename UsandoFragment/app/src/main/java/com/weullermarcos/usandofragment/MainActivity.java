fbb75f906dbef7ada8bc28543b88426a04d39cdd
Weuller Marcos <weullermarcos@MBP-de-Weuller.lan>
2017-09-13 23:50:48 -0300

    funcionando o click

--- UsandoFragment/app/src/main/java/com/weullermarcos/usandofragment/MainActivity.java
+++ UsandoFragment/app/src/main/java/com/weullermarcos/usandofragment/MainActivity.java
@@ -4,10 +4,11 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    LinearLayout lltMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        lltMap = (LinearLayout) findViewById(R.id.lltMap);
+//
+//
+//        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                0.0f
+//        );
+//
+//        lltMap.setLayoutParams(param);
 
 
     }
--- UsandoFragment/app/src/main/java/com/weullermarcos/usandofragment/MapFragment.java
+++ UsandoFragment/app/src/main/java/com/weullermarcos/usandofragment/MapFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.weullermarcos.usandofragment.R;
@@ -32,11 +33,24 @@ public class MapFragment extends Fragment {
 
         btnMarket = (Button) view.findViewById(R.id.btnMarket);
 
+
         btnMarket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Toast.makeText(getActivity().getApplicationContext(), "Meu texto", Toast.LENGTH_LONG).show();
+
+                MainActivity mainActivity = (MainActivity) getActivity();
+
+
+                LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        0.0f
+                );
+
+                mainActivity.lltMap.setLayoutParams(param);
+
             }
         });
 
